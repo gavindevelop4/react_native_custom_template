a5456542c58b9b667422adf0fdaedf31a4a94cea
Gavin Chan <gavindevelop4@gmail.com>
2024-07-16 11:28:07 -0400

    Alter: index screen

---
+++ src/screens/index/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  )
+}
+
+export default IndexScreen

d6ee30b11d7a5331e7f8bad2831ec18e6b474672
Gavin Chan <gavindevelop4@gmail.com>
2024-07-16 11:29:05 -0400

    Alter: provider setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import Router from './router'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 function App(): React.JSX.Element {
   return (
     <>
-      <Router />
+      <Provider store={store}>
+        <Router />
+      </Provider>
     </>
   )
 }
